7fc54f8d32633ed4111ae21754027f4b6c3c1f76
Mobenul Haq <moben.h@outlook.com>
2023-10-03 15:42:58 -0700

    adding base roadmap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>My Roadmap Portfolio</h1>
+
+        <div className="roadmap">
+          <div className="milestone">
+            <h2>2015</h2>
+            <p>Started Computer Science degree at XYZ University.</p>
+          </div>
+
+          <div className="milestone">
+            <h2>2018</h2>
+            <p>Interned at ABC Company. Worked on web applications using React and Node.js.</p>
+          </div>
+
+          <div className="milestone">
+            <h2>2020</h2>
+            <p>Graduated from XYZ University. Joined DEF Corp as a Software Engineer.</p>
+          </div>
+
+          {/* ... Add more milestones as needed ... */}
+        </div>
       </header>
     </div>
   );
