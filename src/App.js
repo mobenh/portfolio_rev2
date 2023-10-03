9787c93c7fcbe7acc204904227ccc686aa86acc2
Mobenul Haq <moben.h@outlook.com>
2023-10-03 15:18:09 -0700

    Removing bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Roadmap from './Roadmap';
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Roadmap/>
         
       </header>
     </div>
