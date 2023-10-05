7418d1b38a59195744a80151b65f39131fa3a803
Mobenul Haq <moben.h@outlook.com>
2023-10-05 14:58:22 -0700

    added dotted line

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 function RoadmapLine({ milestones }) {
   return (
-    <svg className="roadmap-line" viewBox={`0 0 100 ${milestones.length * 100}`}>
+    <svg className="roadmap-line" viewBox={`0 0 100 ${(milestones.length + 2) * 100}`}>
+      <line x1="50" y1="0" x2="50" y2="100" stroke="rgb(43,120,228)" strokeWidth=".5" />
+
       {milestones.map((_, index) => (
-        <line x1="50" y1={index * 100} x2="50" y2={(index + 1) * 100} stroke="rgb(43,120,228)" strokeWidth=".75"/>
+        <line x1="50" y1={(index + 1) * 100} x2="50" y2={(index + 2) * 100} stroke="rgb(43,120,228)" strokeWidth=".5" />
       ))}
+
+      <line x1="50" y1={(milestones.length + 1) * 100} x2="50" y2={(milestones.length + 2) * 100} stroke="rgb(43,120,228)" strokeWidth=".5" />
     </svg>
   );
 }
@@ -20,11 +24,18 @@ function Milestone({ title }) {
 }
 
 function App() {
-  const milestones = ['Skills', 'Projects', '2020']; // This can be a state or derived from props
+  const milestones = ['Skills', 'Projects', '2020'];
+
 
   return (
     <div className="App">
-      <div className="name">Moben Haq</div>
+
+      <div className="name-container">
+        <svg className="dotted-line" viewBox={`0 0 100 20`}>
+          <line x1="50" y1="0" x2="50" y2='20' stroke="rgb(43,120,228)" strokeWidth=".25" strokeDasharray="5,5" />
+        </svg>
+        <div className="name">Moben Haq</div>
+      </div>
 
       <div className="roadmap">
         <RoadmapLine milestones={milestones} />
