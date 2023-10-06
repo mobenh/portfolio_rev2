ec194125aad1e6ac0614f2199c000827c934d616
Mobenul Haq <moben.h@outlook.com>
2023-10-06 08:58:57 -0700

    milestone skeleton

--- src/App.js
+++ src/App.js
@@ -7,31 +7,54 @@ function App() {
     <div className="App">
       <h1 className="name">Moben Haq</h1>
 
-      {/* Vertical line (svg1) */}
       <svg className="svg1-vertical-line">
         <line x1="50%" y1="0" x2="50%" y2="100%" stroke="black" />
-      </svg>
-
-
-
-
-      {/* Horizontal line (svg2) */}
-      <svg className="svg2-horizontal-line">
-        <line x1="50%" y1="50%" x2="10%" y2="50%" stroke="black" />
-      </svg>
-
-      {/* Left vertical line (svg3) */}
-      <svg className="svg3-left-vertical-line">
-        <line x1="20%" y1="0" x2="20%" y2="50%" stroke="black" />
       </svg>
 
       <div className="milestone1">
         Skills
       </div>
 
+      <svg className="svg2-horizontal-line">
+        <line x1="10%" y1="50%" x2="50%" y2="50%" stroke="black" />
+      </svg>
+
+      <svg className="svg3-left-vertical-line">
+        <line x1="20%" y1="0" x2="20%" y2="50%" stroke="black" />
+      </svg>
+
       <div className="milestone2">
         Projects
       </div>
+
+      <svg className="svg4-horizontal-line">
+        <line x1="5.7%" y1="55%" x2="50%" y2="55%" stroke="black" />
+      </svg>
+
+      <div className="milestone3">
+        Experience
+      </div>
+
+      <svg className="svg5-vertical-line">
+        <line x1="87.5%" y1="0" x2="87.5%" y2="50%" stroke="black" />
+      </svg>
+
+      <div className="milestone4">
+        Certifications
+      </div>
+
+      <svg className="svg6-horizontal-line">
+        <line x1="28.5%" y1="115%" x2="50%" y2="115%" stroke="black" />
+      </svg>
+
+      <div className="milestone5">
+        Contact Me
+      </div>
+
+      <svg className="svg7-vertical-line">
+        <line x1="49.9%" y1="62.5%" x2="49.9%" y2="100%" stroke="black" />
+      </svg>
+
     </div>
   );
 }
