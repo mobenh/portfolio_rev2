3bfa025f9032067b1bcb32ed474ca32e90d0736f
Mobenul Haq <moben.h@outlook.com>
2023-10-09 21:18:19 -0700

    added devicons

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ function App() {
       <h1 className="name">Moben Haq</h1>
 
       <svg className="vLine1">
-        <line x1="50%" y1="0" x2="50%" y2="100%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="50%" y1="0" x2="50%" y2="100%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="hLine2">
-        <line x1="10%" y1="50%" x2="50%" y2="50%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="10%" y1="50%" x2="50%" y2="50%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="vLine3">
-        <line x1="20%" y1="-0.2%" x2="20%" y2="50.2%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="20%" y1="-0.2%" x2="20%" y2="50.2%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="hLine4">
-        <line x1="5.7%" y1="55%" x2="50%" y2="55%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="5.7%" y1="55%" x2="50%" y2="55%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="vLine5">
-        <line x1="87.5%" y1="-0.2" x2="87.5%" y2="50.2%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="87.5%" y1="-0.2" x2="87.5%" y2="50.2%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="hLine6">
-        <line x1="28.5%" y1="115%" x2="50%" y2="115%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="28.5%" y1="115%" x2="50%" y2="115%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
       <svg className="vLine7">
-        <line x1="49.9%" y1="62.5%" x2="49.9%" y2="100%" stroke="rgb(43, 120, 228)" stroke-width="3"/>
+        <line x1="49.9%" y1="62.5%" x2="49.9%" y2="100%" stroke="rgb(43, 120, 228)" stroke-width="3" />
       </svg>
 
       <div className="milestone1">
@@ -56,6 +56,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i className="devicon-go-original-wordmark"></i>
       <a href="https://github.com/mobenh/cpp_bootcamp" target="_blank" rel="noopener noreferrer">
         <div className="language2">
           <span className="detailLink">
@@ -63,6 +64,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-cplusplus-plain"></i>
       <a href="https://github.com/mobenh/python_bootcamp" target="_blank" rel="noopener noreferrer">
         <div className="language3">
           <span className="detailLink">
@@ -70,6 +72,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-python-plain"></i>
       <a href="https://github.com/mobenh/javascript-webcrawler" target="_blank" rel="noopener noreferrer">
         <div className="language4">
           <span className="detailLink">
@@ -77,6 +80,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-javascript-plain"></i>
 
       <a href="https://github.com/mobenh/angular-EmployeeCRUD" target="_blank" rel="noopener noreferrer">
         <div className="project1">
@@ -100,6 +104,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-react-original"></i>
       <a href="https://github.com/mobenh/angular-WeatherApp" target="_blank" rel="noopener noreferrer">
         <div className="framework2">
           <span className="detailLink">
@@ -107,6 +112,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-angularjs-plain"></i>
 
       <a href="https://github.com/mobenh/terraform-aws-ec2instance" target="_blank" rel="noopener noreferrer">
         <div className="infrastructure1">
@@ -115,6 +121,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-terraform-plain"></i>
       <a href="https://github.com/mobenh/terraform-aws-ec2instance" target="_blank" rel="noopener noreferrer">
         <div className="infrastructure2">
           <span className="detailLink">
@@ -122,6 +129,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-amazonwebservices-plain-wordmark"></i>
 
       <a href="https://www.credly.com/badges/2be8519a-87bb-4da7-bade-026d50109a5c/public_url" target="_blank" rel="noopener noreferrer">
         <div className="cert1">
@@ -162,6 +170,7 @@ function App() {
           </span>
         </div>
       </a>
+      <i class="devicon-linkedin-plain"></i>
       <a href="https://github.com/mobenh" target="_blank" rel="noopener noreferrer">
         <div className="contact4">
           <span className="detailLink">
@@ -169,39 +178,39 @@ function App() {
           </span>
         </div>
       </a>
-
+      <i class="devicon-github-original"></i>
 
       <svg className="dotted-line-language">
-        <line x1="5.5vw" y1="0%" x2="17vw" y2="0%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="5.5vw" y1="0%" x2="17vw" y2="5%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="5.5vw" y1="0%" x2="17vw" y2="10%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="5.5vw" y1="0%" x2="17vw" y2="15%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="5.5vw" y1="0%" x2="17vw" y2="0%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="5.5vw" y1="0%" x2="17vw" y2="5%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="5.5vw" y1="0%" x2="17vw" y2="10%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="5.5vw" y1="0%" x2="17vw" y2="15%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
       <svg className="dotted-line-project">
-        <line x1="0vw" y1="40%" x2="10vw" y2="20%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="0vw" y1="40%" x2="11vw" y2="30%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="0vw" y1="40%" x2="10vw" y2="20%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="0vw" y1="40%" x2="11vw" y2="30%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
       <svg className="dotted-line-framework">
-        <line x1="35%" y1="0" x2="35%" y2="100%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="35%" y1="0" x2="35%" y2="100%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
       <svg className="dotted-line-infrastructure">
-        <line x1="70%" y1="0" x2="70%" y2="100%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="70%" y1="0" x2="70%" y2="100%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
       <svg className="dotted-line-cert">
-        <line x1="55vw" y1="55%" x2="40vw" y2="40%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="55vw" y1="55%" x2="40vw" y2="50%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="55vw" y1="55%" x2="40vw" y2="60%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="55vw" y1="55%" x2="40vw" y2="40%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="55vw" y1="55%" x2="40vw" y2="50%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="55vw" y1="55%" x2="40vw" y2="60%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
       <svg className="dotted-line-contact">
-        <line x1="26vw" y1="75%" x2="13vw" y2="60%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="26vw" y1="75%" x2="14vw" y2="70%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="26vw" y1="75%" x2="12vw" y2="80%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
-        <line x1="26vw" y1="75%" x2="12vw" y2="90%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2"/>
+        <line x1="26vw" y1="75%" x2="13vw" y2="60%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="26vw" y1="75%" x2="14vw" y2="70%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="26vw" y1="75%" x2="12vw" y2="80%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
+        <line x1="26vw" y1="75%" x2="12vw" y2="90%" stroke="rgb(43, 120, 228)" strokeDasharray="4,5" stroke-width="2" />
       </svg>
 
     </div>
